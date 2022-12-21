4b4f497ab8d9adbc9a9f92c3126b3dbe11ad62c6
manuelcampagnolo <mlc@isa.ulisboa.pt>
2022-12-21 16:57:36 +0000

    Update urwagwa.py

--- urwagwa.py
+++ urwagwa.py
@@ -6,7 +6,7 @@ x=np.array([0.880, 1.054, 0.866, 0.883, 0.913, 0.893, 0.872, 0.949, 0.848, 0.907
 y=np.array([0.894, 1.113, 0.908, 0.946, 0.976, 0.929, 0.870, 0.941, 0.864, 0.990, 0.845, 0.930, 1.010, 0.871, 0.974, 0.917])
 
 # teste shapiro
-print(stats.shapiro(x-y))
+stats.shapiro(x-y)
 
 # t.test para amostras emparelhadas
 stats.ttest_rel(x,y,alternative="less")
